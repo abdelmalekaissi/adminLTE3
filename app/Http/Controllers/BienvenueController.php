690c39dbe319cf575906ec643002da990adee275
Abdelmalek Aissi <aissiabdelmalek@gmail.com>
2020-05-09 00:16:11 +0200

    This is a simple implementation of AdminLTE.
    Up to this point
    https://youtu.be/Kljm9P7JZbI?t=1143

---
+++ app/Http/Controllers/BienvenueController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BienvenueController extends Controller
+{
+    
+    public function bienvenue()
+    {
+        return view('/bienvenue');
+    }
+}
